f466e2988c717f0c50e78841ae4f6e7cb62d3a7e
Ryan Bey <beycommaryan@gmail.com>
2025-09-15 14:29:22 -0600

    init

---
+++ src/Favorites.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Favorites(params) {
+  return (
+    <p>Favorites Component</p>
+  )
+}
---
+++ src/PokemonList.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function PokemonList() {
+  return (
+    <p>Pokemon List Component</p>
+  )
+}
